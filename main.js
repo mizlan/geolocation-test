613dc8f58dc1b5b54df91ea1152d28fa9785dd7e
Michael Lan <michaellan202@gmail.com>
2021-04-04 15:38:32 -0700

    stuff

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 // the AllergyEats API needs geographical coordinates (latitude and longitude)
 // embedded in the URL as parameters
 
+userLocation = {
+  latitude: undefined,
+  longitude: undefined,
+  denied: false,
+  loaded: false
+}
+
+// use Geolocation API:
+// https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates
+// to get coordinates
+function getPosition(options) {
+  return new Promise((resolve, reject) => 
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  );
+}
+
+let locationFetch = getPosition().then((position) => {
+  userLocation.latitude  = position.coords.latitude;
+  userLocation.longitude = position.coords.longitude;
+  userLocation.loaded = true;
+  document.getElementById('restaurant-info').innerHTML = 'location loaded';
+}).catch((err) => {
+  if (err.message.includes('denied')) {
+    denied = true;
+  }
+  infoBox.innerHTML = 'location denied';
+});
+
+function locationHasLoaded() {
+  return userLocation.loaded
+}
+
 // supported allergens:
 // - Peanuts
 // - Dairy
@@ -12,48 +44,40 @@
 // - Gluten
 // - Shellfish
 // - Soy
-function buildQuery(info, allergens) {
-  // use Geolocation API:
-  // https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates
-  // to get coordinates
-  // allergenString = allergens.join('%2C'); // URI-encoded comma
+async function buildQuery(info, allergens) {
+
+  await locationFetch;
+  info.innerHTML = 'getting nearby restaurants...'
+
   allergenString = allergens.join(',');
 
-  // after we retrieve the URL
-  function onRetrieve(position) {
-    info.innerHTML = 'fetching data'
-    const latitude  = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(longitude, latitude);
-    url = `https://api.allergyeats.com/api/v1/restaurant/query?take=20&allergynames=${allergenString}&distance=20&location=${latitude}%2C${longitude}`
+  longitude = userLocation.longitude;
+  latitude = userLocation.latitude;
+  url = `https://api.allergyeats.com/api/v1/restaurant/query?take=20&allergynames=${allergenString}&distance=20&location=${latitude}%2C${longitude}`
 
-    fetch(url).then((res) => {
-      let x = res.json()
-      console.log(x);
-    }).catch(() => {
-      info.innerHTML = 'could not get data';
-    });
-  }
-  function onError() {
-    // tell the user their location could not be retrieved
-  }
-
-  info.innerHTML = 'getting location...'
-  navigator.geolocation.getCurrentPosition(onRetrieve, onError);
-
+  fetch(url).then(async (res) => {
+    let data = await res.json();
+    info.innerHTML = JSON.stringify(data);
+    console.log(data);
+  }).catch(() => {
+    info.innerHTML = 'could not get data';
+  });
 }
 
-// wait till the HTML loads
 window.onload = function() {
 
-  // get the button element in our code
   const button = document.getElementById('restaurants');
   const infoBox = document.getElementById('restaurant-info');
 
-  // add a listener so that every time the button is clicked,
-  // something happens
+  if (locationHasLoaded()) {
+    infoBox.innerHTML = 'location loaded'
+  } else if (userLocation.denied) {
+    infoBox.innerHTML = 'location denied, cannot get nearby restaurants'
+  } else {
+    infoBox.innerHTML = 'getting location...'
+  }
+
   button.addEventListener('click', () => {
     buildQuery(infoBox, ['Peanuts', 'Tree Nuts']);
   });
-
 }
