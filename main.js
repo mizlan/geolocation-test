ee08e603ecc0b2d4141f82685b04faaa95e11794
Michael Lan <michaellan202@gmail.com>
2021-04-04 10:01:53 -0700

    info

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 // - Gluten
 // - Shellfish
 // - Soy
-function buildQuery(allergens) {
+function buildQuery(info, allergens) {
   // use Geolocation API:
   // https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates
   // to get coordinates
@@ -21,18 +21,24 @@ function buildQuery(allergens) {
 
   // after we retrieve the URL
   function onRetrieve(position) {
-    document.getElementById('restaurant-info').innerHTML = 'fetching data'
+    info.innerHTML = 'fetching data'
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log(longitude, latitude);
     url = `https://api.allergyeats.com/api/v1/restaurant/query?take=20&allergynames=${allergenString}&distance=20&location=${latitude}%2C${longitude}`
-    // fetch(url);
+
+    fetch(url).then((json) => {
+      console.log(json);
+      info.innerHTML = json;
+    }).error(() => {
+      info.innerHTML = 'could not get data';
+    });
   }
   function onError() {
     // tell the user their location could not be retrieved
   }
 
-  document.getElementById('restaurant-info').innerHTML = 'getting location...'
+  info.innerHTML = 'getting location...'
   navigator.geolocation.getCurrentPosition(onRetrieve, onError);
 
 }
@@ -42,11 +48,12 @@ window.onload = function() {
 
   // get the button element in our code
   const button = document.getElementById('restaurants');
+  const infoBox = document.getElementById('restaurant-info');
 
   // add a listener so that every time the button is clicked,
   // something happens
   button.addEventListener('click', () => {
-    buildQuery(['Peanuts', 'Tree Nuts']);
+    buildQuery(infoBox, ['Peanuts', 'Tree Nuts']);
   });
 
 }
